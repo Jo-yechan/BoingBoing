fcd6eae0f01885f9ff6678f830e33b6162824480
gksqls1103 <gksqls1103@naver.com>
2026-01-20 11:04:51 +0900

    테스트코드

--- servo.h
+++ servo.h
@@ -2,8 +2,20 @@
 #ifndef SERVO_H
 #define SERVO_H
 
+
 #define DEVICE_PATH "/dev/servo"
 
+// [핵심] C++ 컴파일러(g++)가 볼 때만 이 부분이 작동하도록 함
+#ifdef __cplusplus
+extern "C" {
+#endif
+
+// 여기에 C 함수들을 선언합니다
 int set_servo_angles(int fd, int value1, int value2);
 
-#endif // SERVO_H
+// [핵심] C++일 때 닫는 괄호
+#ifdef __cplusplus
+}
+#endif
+
+#endif
